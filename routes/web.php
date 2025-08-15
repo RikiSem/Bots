006a9ceabfe8790ff641e65bf3cc4fa741412cdd
evaskes <evaskes@team.amocrm.com>
2025-08-15 20:01:20 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Bots\TaroBot\src\Controllers\IndexController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'show']);
